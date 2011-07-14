641c49284c712110a9ca91bcc63cec706fa60636
Koen Hoeymans <koenhoeymans@gmail.com>
2011-07-14 17:16:05 +0200

    added inline code

---
+++ UnitTests/Patterns/CodeInlineTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once dirname(__FILE__)
+	. DIRECTORY_SEPARATOR . '..' 
+	. DIRECTORY_SEPARATOR . 'TestHelper.php';
+
+class Vidola_Patterns_CodeInlineTest extends PHPUnit_Framework_TestCase
+{
+	public function setup()
+	{
+		$this->codeInline = new \Vidola\Patterns\CodeInline();
+	}
+
+	/**
+	 * @test
+	 */
+	public function transformsCodeBetweenBackticks()
+	{
+		$this->assertEquals(
+			'Text with <code>code</code> in between.',
+			$this->codeInline->replace('Text with ´code´ in between.')
+		);
+	}
+}
---
+++ Vidola/Patterns/CodeInline.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @package Vidola
+ */
+namespace Vidola\Patterns;
+
+/**
+ * @package Vidola
+ */
+class CodeInline implements Pattern
+{
+	public function replace($text)
+	{
+		return preg_replace(
+			'#´(.+)?´#',
+			"<code>\${1}</code>",
+			$text
+		);
+	}
+}
