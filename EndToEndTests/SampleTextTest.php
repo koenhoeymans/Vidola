28e8af50cbf39628870fe929f2ca3bbe7dd2d61e
Koen Hoeymans <koenhoeymans@gmail.com>
2011-07-26 16:08:56 +0200

    intentions of end to end tests more clear

--- EndToEndTests/SampleTextTest.php
+++ EndToEndTests/SampleTextTest.php
@@ -2,7 +2,7 @@
 
 require_once('TestHelper.php');
 
-class Vidola_EndToEndTests_FileCreationTest extends PHPUnit_Framework_TestCase
+class Vidola_EndToEndTests_SampleTextTest extends PHPUnit_Framework_TestCase
 {
 	public function setup()
 	{
@@ -10,14 +10,6 @@ class Vidola_EndToEndTests_FileCreationTest extends PHPUnit_Framework_TestCase
 		if (file_exists($dir . 'SampleText.html'))
 		{
 			unlink($dir . 'SampleText.html');
-		}
-		if (file_exists($dir . 'Index.html'))
-		{
-			unlink($dir . 'Index.html');
-		}
-		if (file_exists($dir . 'OtherDocument.html'))
-		{
-			unlink($dir . 'OtherDocument.html');
 		}
 	}
 
@@ -50,33 +42,6 @@ class Vidola_EndToEndTests_FileCreationTest extends PHPUnit_Framework_TestCase
 			file_get_contents(
 				$_SERVER['argv']['target.dir']
 				. DIRECTORY_SEPARATOR . 'SampleText.html'
-			)
-		);
-	}
-
-	/**
-	 * @test
-	 */
-	public function directoryAsInputTransformsIndex()
-	{
-		// given
-		$_SERVER['argv']['source'] = __DIR__
-			. DIRECTORY_SEPARATOR . 'Support'
-			. DIRECTORY_SEPARATOR . 'FolderWithTwoDocuments';
-		$_SERVER['argv']['target.dir'] = sys_get_temp_dir();
-
-		// when
-		\Vidola\Vidola::run();
-
-		// then
-		$this->assertTrue(
-			file_exists(
-				sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'Index.html'
-			)
-		);
-		$this->assertFalse(
-			file_exists(
-				sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'OtherDocument.html'
 			)
 		);
 	}
