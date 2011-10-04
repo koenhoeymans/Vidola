3b31e4198c4e9c9375e14e40476fa83dfb88a684
Koen Hoeymans <koenhoeymans@gmail.com>
2011-10-04 16:27:37 +0200

    added CodeBlocks test

--- EndToEndTests/MarkdownTest.php
+++ EndToEndTests/MarkdownTest.php
@@ -81,4 +81,12 @@ class Vidola_EndToEndTests_MarkdownTest extends PHPUnit_Framework_TestCase
 	{
 		$this->createTestFor('BlockquotesWithCodeBlocks');
 	}
+
+	/**
+	 * @test
+	 */
+	public function codeBlocks()
+	{
+		$this->createTestFor('CodeBlocks');
+	}
 }
