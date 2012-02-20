936e4bbe5778eeeaf3165c7af56f55ae433cbe93
Koen Hoeymans <koenhoeymans@gmail.com>
2012-02-20 07:50:58 +0100

    git conflict resolving

--- UnitTests/TextReplacer/RecursiveReplacer/RecursiveReplacerTest.php
+++ UnitTests/TextReplacer/RecursiveReplacer/RecursiveReplacerTest.php
@@ -33,11 +33,7 @@ class Vidola_TextReplacer_TextToHtmlReplacer_RecursiveReplacerTest extends PHPUn
 			->expects($this->atLeastOnce())
 			->method('getPatterns')
 			->will($this->returnValue(array()));
-<<<<<<< HEAD
-		$preProcessor = $this->getMock('\\Vidola\\Processor\\Processor');
-=======
 		$preProcessor = $this->getMock('\\Vidola\\Processor\\TextProcessor');
->>>>>>> tryout
 		$preProcessor
 			->expects($this->atLeastOnce())
 			->method('process')
@@ -56,11 +52,7 @@ class Vidola_TextReplacer_TextToHtmlReplacer_RecursiveReplacerTest extends PHPUn
 			->expects($this->atLeastOnce())
 			->method('getPatterns')
 			->will($this->returnValue(array()));
-<<<<<<< HEAD
-		$postProcessor = $this->getMock('\\Vidola\\Processor\\Processor');
-=======
 		$postProcessor = $this->getMock('\\Vidola\\Processor\\TextProcessor');
->>>>>>> tryout
 		$postProcessor
 			->expects($this->atLeastOnce())
 			->method('process')
