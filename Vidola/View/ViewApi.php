43b28bb61911916c66cc0d25e118d7b863e69322
Koen Hoeymans <koenhoeymans@gmail.com>
2012-09-07 07:39:32 +0200

    doc update

--- Vidola/View/ViewApi.php
+++ Vidola/View/ViewApi.php
@@ -12,6 +12,8 @@ interface ViewApi
 {
 	/**
 	 * The name by which the API can be found in the view, eg $user for name 'user'.
+	 * The view can allow a template to access this API and can distinguish between
+	 * different ones by their name.
 	 */
 	public function getName();
 }
