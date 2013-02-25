dbe52540db2e135af25630ada1c662ca52f06694
Koen Hoeymans <koenhoeymans@gmail.com>
2013-02-25 07:39:45 +0100

    introduced HtmlFileUrlBuilder from AnyMark

---
+++ Vidola/Util/HtmlFileUrlBuilder.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @package Vidola
+ */
+namespace Vidola\Util;
+
+/**
+ * @package Vidola
+ */
+class HtmlFileUrlBuilder implements \AnyMark\Util\InternalUrlBuilder
+{
+	/**
+	 * @see AnyMark\Util.InternalUrlBuilder::createLink()
+	 */
+	public function createRelativeLink($to, $relativeTo = null)
+	{
+		$numberSignPos = strpos($to, "#");
+
+		if ($numberSignPos === false)
+		{
+			$filePart = $to;
+			$relPart = '';
+		}
+		else
+		{
+			$filePart = substr($to, 0, $numberSignPos);
+			$relPart = substr($to, $numberSignPos);
+		}
+
+		$levelsUp = $this->howManyLevelsDeep($relativeTo);
+		while ($levelsUp>0)
+		{
+			$filePart = '../' . $filePart;
+			$levelsUp--;
+		}
+
+		$info = pathinfo($filePart);
+		if (!isset($info['extension']))
+		{
+			$filePart = $this->addExtension($filePart);
+		}
+
+		return $filePart . $relPart;
+	}
+
+	private function howManyLevelsDeep($resource)
+	{
+		return count(explode(DIRECTORY_SEPARATOR, $resource)) -1;
+	}
+
+	private function addExtension($resource)
+	{
+		return $resource . '.html';
+	}
+}
